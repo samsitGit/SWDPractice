6a3576d384c6c3a3c7b17ae2467500d674764677
Sam Sit <ss4521@rit.edu>
2023-02-06 13:03:13 -0500

    Update variables.py

--- Unit 2/variables.py
+++ Unit 2/variables.py
@@ -5,4 +5,15 @@ def variable_practice():
     first5DigitsOfPi = 14159
     print(ageInMonths, numberOfDaysInAYear, nameOfFirstPet, first5DigitsOfPi)
 
+def expressions_practice():
+    literal = 5
+    addition = 5+2
+    exponent = 5**2
+    floorDivision = 5/2
+    mod = 5%2
+    para = (5+2)*2
+    mix = (5**2*2+2)/2
+    print(literal, addition, exponent, floorDivision, mod, para, mix)
+
 variable_practice()
+expressions_practice()
