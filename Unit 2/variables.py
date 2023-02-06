e1432ad7dd651e4865eca1c52c53056acf214a46
Sam Sit <ss4521@rit.edu>
2023-02-06 13:09:04 -0500

    Update variables.py

--- Unit 2/variables.py
+++ Unit 2/variables.py
@@ -1,8 +1,8 @@
 def variable_practice():
-    ageInMonths = 268
-    numberOfDaysInAYear = 365
-    nameOfFirstPet = "Maple"
-    first5DigitsOfPi = 14159
+    ageInMonths = int(268)
+    numberOfDaysInAYear = int(365)
+    nameOfFirstPet = str("Maple")
+    first5DigitsOfPi = int(14159)
     print(ageInMonths, numberOfDaysInAYear, nameOfFirstPet, first5DigitsOfPi)
 
 def expressions_practice():
@@ -15,5 +15,15 @@ def expressions_practice():
     mix = (5**2*2+2)/2
     print(literal, addition, exponent, floorDivision, mod, para, mix)
 
+#asks users for two numbers and print the results of operations
+def prompt_and_print():
+    num1 = float(input("first number: "))
+    num2 = float(input("second number: "))
+    print(num1+num2)
+    print(num1-num2)
+    print(num1*num2)
+    print(num1/num2)
+
 variable_practice()
 expressions_practice()
+prompt_and_print()
