5c7d4850f96b295542876f937b14311e3fa0e009
Sam Sit <ss4521@rit.edu>
2023-02-06 12:54:48 -0500

    Update hello.py

--- Unit 2/hello.py
+++ Unit 2/hello.py
@@ -1,9 +1,14 @@
-def helloWorld():
-    print("Hello, World!")
+"""
+This program prints a hello world and customized message to the user.
+Author: Sam Sit
+"""
 
-def helloYou():
-    name = input("What is your name? :")
-    print("Hello, ", name, "!", sep="")
+def helloWorld(): #define function
+    print("Hello, World!") #prints string
 
-helloWorld()
-helloYou()
+def helloYou(): #define function
+    name = input("What is your name? :") #print, ask for input, and save in variable
+    print("Hello, ", name, "!", sep="") #prints string formatted
+
+helloWorld() #runs the first function
+helloYou() #runs the second function
