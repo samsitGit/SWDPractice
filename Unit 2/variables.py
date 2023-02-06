87b3e2474d110d25b9cbd34f05acaed918ccb72e
Sam Sit <ss4521@rit.edu>
2023-02-06 12:57:50 -0500

    Create variables.py

---
+++ Unit 2/variables.py
@@ -0,0 +1,8 @@
+def variable_practice():
+    ageInMonths = 268
+    numberOfDaysInAYear = 365
+    nameOfFirstPet = "Maple"
+    first5DigitsOfPi = 14159
+    print(ageInMonths, numberOfDaysInAYear, nameOfFirstPet, first5DigitsOfPi)
+
+variable_practice()
