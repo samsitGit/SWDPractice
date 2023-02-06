f808025ddc216aec8f9819af55838bf14544b364
Sam Sit <ss4521@rit.edu>
2023-02-06 12:49:11 -0500

    Update happy_birthday.py

--- Unit 2/happy_birthday.py
+++ Unit 2/happy_birthday.py
@@ -1,6 +1,11 @@
-name = input("name: ")
-birthMonth = input("Birth Month: ")
-birthDay = input("Birth Day of the Month: ")
-birthYear = input("Birth Year: ")
-print(name, ", your birthday is on ", sep="", end="")
-print(birthMonth, " ", birthDay, ", ", birthYear, "!", sep="")
+def bday_message():
+    name = input("name: ")
+    birthMonth = input("Birth Month: ")
+    birthDay = input("Birth Day of the Month: ")
+    birthYear = input("Birth Year: ")
+    print(name, ", your birthday is on ", sep="", end="")
+    print(birthMonth, " ", birthDay, ", ", birthYear, "!", sep="")
+
+bday_message()
+bday_message()
+bday_message()
