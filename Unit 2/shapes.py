d29be4b485ce00e10a7b9513074f38b9f6b00812
Sam Sit <ss4521@rit.edu>
2023-02-06 13:38:30 -0500

    Create shapes.py

---
+++ Unit 2/shapes.py
@@ -0,0 +1,14 @@
+'''
+    Practice for functions in HW 2.2 - #4
+    Author: Sam Sit
+'''
+
+def areaCircle():
+    radius = float(input("Circle: r = "))
+    
+    print(", area =", (3.14*radius**2))
+
+def main():
+    areaCircle()
+
+main()
