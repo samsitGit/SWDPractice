94c411b4c5b69d0d235f1497a9ec0830ba4575b3
Sam Sit <ss4521@rit.edu>
2023-02-06 12:47:05 -0500

    Create happy_birthday.py

---
+++ Unit 2/happy_birthday.py
@@ -0,0 +1,6 @@
+name = input("name: ")
+birthMonth = input("Birth Month: ")
+birthDay = input("Birth Day of the Month: ")
+birthYear = input("Birth Year: ")
+print(name, ", your birthday is on ", sep="", end="")
+print(birthMonth, " ", birthDay, ", ", birthYear, "!", sep="")
