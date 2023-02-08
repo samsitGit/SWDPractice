fafe08fcf245cfbfd0a26d111e276a9a966747b2
Sam Sit <ss4521@rit.edu>
2023-02-08 14:44:10 -0500

    Create calculator.py
    3.2

---
+++ Unit 3/calculator.py
@@ -0,0 +1,26 @@
+'''
+    3.2 Powerpoint
+    Author: Sam Sit
+'''
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def main():
+    x = int(input("Enter x: "))
+    y = int(input("Enter y: "))
+    print(add(x,y))
+    print(subtract(x,y))
+    print(multiply(x,y))
+    print(divide(x,y))
+
+main()
