3b789693e66ad930cd1b2749a2f835f51c5360e3
Sam Sit <ss4521@rit.edu>
2023-02-08 13:39:23 -0500

    Update smiley.py
    completed smiley

--- Unit 3/smiley.py
+++ Unit 3/smiley.py
@@ -24,16 +24,61 @@ def draw_centered_circle(x, y, radius, color):
     turtle.forward(radius)
     turtle.left(180)
     
-def draw_smiley():
-    draw_centered_circle(0, 0, 100, "yellow")
-    draw_centered_circle(0, 0, 20, "pink")
+def draw_smiley(x, y, face_size, eye_color):
+
+    eye_x = x + face_size * .35
+    eye_y = x + face_size * .25
+    eye_size = .25 * face_size
+    mouth_x = x
+    mouth_y = x + face_size * -.25
+    mouth_size = .6*face_size
+
+    draw_centered_circle(x, y, face_size, "yellow")
+    draw_centered_circle(x, y, .1*face_size, "pink")
+    draw_eye(-(eye_x), eye_y, eye_size, eye_color)
+    draw_eye(eye_x, eye_y, eye_size, eye_color)
+    draw_mouth(mouth_x, mouth_y, mouth_size, "black")
+
+def tweak():
+    turtle.speed(1)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.tracer(False)
+    turtle.circle(50)
+    turtle.tracer(True)
+    turtle.hideturtle()
+
+def draw_eye(x, y, radius, iris_color):
+    draw_centered_circle(x, y, radius, "white")
+    draw_centered_circle(x, y, .5*radius, iris_color)
+    draw_centered_circle(x, y, .25*radius, "black")
+
+def draw_mouth(x, y, radius, fill_color):
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.forward(-(radius))
+    turtle.right(90)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.fillcolor(fill_color)
+    turtle.circle(radius, 180)
+    turtle.end_fill()
+    turtle.right(90)
+    turtle.up()
+    turtle.forward(radius)
+    turtle.right(180)
 
 def main():
+
     #draw_circle(0, 0, 100, "white")
     #draw_circle(-200, -20, 30, "yellow")
     #draw_circle(200, -20, 50, "blue")
     #draw_centered_circle(0, 0, 50, "blue")
-    draw_smiley()
+    #draw_smiley()
+    #tweak()
+
+    draw_smiley(0, 0, 150, "blue")
+
     input("Press enter to continue...")
 
 main()
