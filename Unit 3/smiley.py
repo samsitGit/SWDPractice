13310b3181604bb346de2c05183dde3f33b10d24
Sam Sit <ss4521@rit.edu>
2023-02-08 13:16:21 -0500

    Update smiley.py

--- Unit 3/smiley.py
+++ Unit 3/smiley.py
@@ -9,11 +9,31 @@ def draw_circle(x, y, radius, color):
     turtle.circle(radius)
     turtle.end_fill()
 
+def draw_centered_circle(x, y, radius, color):
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.forward(radius) #go radius away and draw circle from there
+    turtle.left(90)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.fillcolor(color)
+    turtle.circle(radius)
+    turtle.end_fill()
+    turtle.left(90)
+    turtle.up()
+    turtle.forward(radius)
+    turtle.left(180)
+    
+def draw_smiley():
+    draw_centered_circle(0, 0, 100, "yellow")
+    draw_centered_circle(0, 0, 20, "pink")
 
 def main():
-    draw_circle(0, 0, 100, "white")
-    draw_circle(-200, -20, 30, "yellow")
-    draw_circle(200, -20, 50, "blue")
+    #draw_circle(0, 0, 100, "white")
+    #draw_circle(-200, -20, 30, "yellow")
+    #draw_circle(200, -20, 50, "blue")
+    #draw_centered_circle(0, 0, 50, "blue")
+    draw_smiley()
     input("Press enter to continue...")
 
 main()
