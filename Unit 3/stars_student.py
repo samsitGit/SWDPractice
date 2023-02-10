2694c113cd057afc228cf16f6a96a0ed3e7fdcca
Sam Sit <ss4521@rit.edu>
2023-02-10 13:06:01 -0500

    Update stars_student.py
    hw file

--- Unit 3/stars_student.py
+++ Unit 3/stars_student.py
@@ -1,4 +1,93 @@
-'''
-    3.2A HW
-    Author: Sam Sit
-'''
+"""
+Draws a sky filled with stars and planets.
+HW3.2
+@author Sam Sit
+"""
+
+import random
+import turtle
+
+def tweak(speed, tracer):
+    """
+    Adjust the turtle's speed and tracer settings so that it can be sped up
+    or slowed down as needed for visual debugging.
+    """
+    turtle.speed(speed)
+    turtle.tracer(tracer)
+
+def random_yellow():
+    """
+    Sets the turtle's fill color to a random shade of yellow using RGB values.
+    """
+    # the random.random() function returns a floating point value between
+    # 0.1 and 1.0. This expression guarantees that the red value will be
+    # between 0.5 and 1.0.
+    red = 0.5 + random.random() * 0.5
+    green = red
+    blue = red / 2
+
+    # the turtle color can be set using RGB values ranging from 0.0 to 1.0. In
+    #  this case the red and green values are between 0.5 and 1.0 and the blue
+    # value is half the value - this guarantees a shade of yellow.
+    turtle.color(red, green, blue)
+    turtle.fillcolor(red, blue, green)
+
+def random_move():
+    """
+    Moves the turtle to a random location and orientation on the screen.
+    """
+    x = random.randint(-1000, 1000)
+    y = random.randint(-1000, 1000)
+    turtle.goto(x, x) 
+
+    angle = random.randint(0, 360)
+
+def draw_star(length):
+    """
+    Draws a star at the turtle's current location and orientation.
+    """
+    random_yellow()
+
+    turtle.down()
+    turtle.begin_fill()
+
+    turtle.forward(length)
+    turtle.left(36)
+    turtle.forward(length)
+    turtle.right(108)
+
+    turtle.forward(length)
+    turtle.left(36)
+    turtle.forward(length)
+    turtle.right(108)
+
+    turtle.forward(length)
+    turtle.left(36)
+    turtle.forward(length)
+    turtle.right(108)
+
+    turtle.forward(length)
+    turtle.left(36)
+    turtle.forward(length)
+    turtle.right(108)
+
+    turtle.forward(length)
+    turtle.left(36)
+    turtle.forward(length)
+    turtle.right(108)
+
+    turtle.end_fill()    
+
+def main():
+    """
+    Should ultimately draw a night sky filled with stars and planets.
+    """
+    turtle.bgcolor("black")
+    tweak(True, 1)
+    length = input("Enter length of star to draw (e.g. 100): ")
+    draw_star(length)
+    tweak(True, 1)
+    turtle.hide()
+    input("Press enter to continue...")
+
+main()
