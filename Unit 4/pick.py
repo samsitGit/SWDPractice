2ca3180499cb9f64123a041c1567bba65efaa122
Sam Sit <ss4521@rit.edu>
2023-02-13 13:13:44 -0500

    testing 4.1

--- Unit 4/pick.py
+++ Unit 4/pick.py
@@ -1,10 +1,49 @@
+'''
+    4.1 Powerpoint
+    Author: Sam Sit
+    Must run "python -m pytest"
+'''
+
 import random
 
 def is_correct(guess, answer):
-    return guess == answer
+    #return guess == answer
+    if guess == answer:
+        print("You guessed the number!")
+    
+    else:
+        print("Your guess was", int(check_guess(guess, answer)), "away from the number.")
+
+def isEquilateral (a, b, c):
+    if a == b and b == c and c == a:
+        print("Yes")
+    else:
+        print("No")
+
+def check_guess(guess, answer):
+    if guess == answer:
+        return 0
+    else:
+        difference = ((guess - answer)**2)**.5
+        return difference
+
+def test_check_guess_correct():
+    assert check_guess(5, 5) == 0
+
+#def test_check_guess_incorrect():
+#    assert (check_guess(4, 5)) > 0
+#    assert (check_guess(5, 4)) > 0
+
+def test_check_guess_too_high():
+    assert (check_guess(5, 4)) > 0
+
+def test_check_guess_too_low():
+    assert (check_guess(4, 5)) > 0
+
 def main():
     answer = random.randint(1,10)
-    guess = input("Guess a number between 1-10: ")
-    print(is_correct(guess, answer))
+    guess = int(input("Guess a number between 1-10: "))
+    #print(is_correct(guess, answer))
+    is_correct(guess, answer)
 
 main()
---
+++ Unit 4/test_pick.py
@@ -0,0 +1,19 @@
+'''
+    4.1 Powerpoint
+    Author: Sam Sit
+    Must run "python -m pytest"
+'''
+
+import pick
+
+def test_check_guess_correct():
+    assert pick.check_guess(5, 5) == 0
+
+def test_check_guess_too_high():
+    assert (pick.check_guess(5, 4)) > 0
+
+def test_check_guess_too_low():
+    assert (pick.check_guess(4, 5)) > 0
+
+if __name__ == "__main__":
+    main()
