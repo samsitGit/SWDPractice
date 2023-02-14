85348af3d12c3d7a0d34703e869f4aad40de719e
Sam Sit <ss4521@rit.edu>
2023-02-14 16:10:53 -0500

    pass test fr

--- Unit 4/pixart.py
+++ Unit 4/pixart.py
@@ -12,16 +12,30 @@ COLUMNS = 20
 def initialize():
     turtle.reset()
     turtle.bgcolor("blue")
-    turtle.tracer(0)
+    turtle.speed(0)
     turtle.up()
     turtle.goto(-300, 300)
     turtle.pensize(1)
     turtle.fillcolor("white")
     turtle.pencolor("black")
-    turtle.down()
 
 def draw_black_pixel():
+    turtle.pencolor("black")
     turtle.fillcolor("black")
+    turtle.begin_fill()
+    turtle.down()
+    times = 0
+    while times < 4:
+        times = times + 1
+        turtle.forward(PIXEL_SIZE)
+        turtle.right(90)
+    turtle.up()
+    turtle.end_fill()
+    turtle.forward(PIXEL_SIZE)
+
+def draw_red_pixel():
+    turtle.pencolor("black")
+    turtle.fillcolor("red")
     turtle.begin_fill()
     turtle.down()
     times = 0
@@ -31,7 +45,7 @@ def draw_black_pixel():
         turtle.right(90)
     turtle.up()
     turtle.end_fill()
-    #turtle.goto(0, 0)
+    turtle.forward(PIXEL_SIZE)
 
 def main():
     initialize()
--- Unit 4/pixart_test.py
+++ Unit 4/pixart_test.py
@@ -2,13 +2,20 @@ import turtle
 import pixart
 
 def test_initialize():
-    assert (-300, turtle.xcor())
+    pixart.initialize()
+#    assert (-300 == turtle.xcor())
+    assertle(0, -300, 300, False, "black", "white")
 
 def test_black_pixel():
-    pixart.initialize()
     pixart.draw_black_pixel()
-    assert (-270, turtle.xcor())
-    assert (300, turtle.ycor())
-    assert (False, turtle.isdown())
-    assert ("black", turtle.pencolor())
-    assert ("black", turtle.fillcolor())
+    assertle(0, -270, 300, False, "black", "black")
+
+
+
+def assertle(speed, x, y, state, pen, fill):
+    assert (speed == turtle.speed())
+    assert (x == turtle.xcor())
+    assert (y == turtle.ycor())
+    assert (state == turtle.isdown())
+    assert (pen == turtle.pencolor())
+    assert (fill == turtle.fillcolor())
