40cca48ad639f9f0845c238a4bf1a7895f5779ce
Sam Sit <ss4521@rit.edu>
2023-02-14 16:23:37 -0500

    Update loops.py
    practice escape

--- Unit 4/loops.py
+++ Unit 4/loops.py
@@ -11,7 +11,16 @@ def countdown(number):
         number = number - 1
     print("sum: " + str(sum))
 
+def quotable():
+    print("She said \"I don't like broccoli\".")
+    print("A\tB\tC\tD\tE")
+    print("This/is\\a/test\\.")
+    print("This\nis a string\nwith newlines in\nthe middle.")
+    print("She\t\tsaid \"I \\\ndon't\t\tlike \\\nbroccoli.\"")
+
+
 def main():
     countdown(5)
+    quotable()
 
 main()
