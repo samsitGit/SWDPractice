a0ab1f11184eba2516532f06bf33e00383fe680c
Sam Sit <ss4521@rit.edu>
2023-02-14 16:29:18 -0500

    Update loops.py
    practice loops

--- Unit 4/loops.py
+++ Unit 4/loops.py
@@ -18,9 +18,25 @@ def quotable():
     print("This\nis a string\nwith newlines in\nthe middle.")
     print("She\t\tsaid \"I \\\ndon't\t\tlike \\\nbroccoli.\"")
 
+def printSepLine(string):
+    print(string)
+    count = 0
+    while count < 4:
+        print(string[count])
+        count = count + 1
+
+def printSepLineReverse(string):
+    print(string)
+    count = 3
+    while count >= 0:
+        print(string[count])
+        count = count - 1
+
 
 def main():
-    countdown(5)
-    quotable()
+    #countdown(5)
+    #quotable()
+    #printSepLine("Yoda")
+    printSepLineReverse("Yoda")
 
 main()
