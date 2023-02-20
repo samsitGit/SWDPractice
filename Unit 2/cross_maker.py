d19e9895251586ca1da51e97df454208a0ea863a
Sam Sit <ss4521@rit.edu>
2023-02-20 12:41:27 -0500

    add write word and decode command

--- Unit 2/cross_maker.py
+++ Unit 2/cross_maker.py
@@ -24,13 +24,37 @@ def initialize():
 def write_letter(letter):
     turtle.write(letter, align='center', font=("Arial", 19, "normal"))
 
+def write_word(col, row, word, axis):
+    if axis == "h":
+        count = 0
+        for i in range(len(word)):
+            move(col+count, row)
+            count += 1
+            write_letter(word[i])
+    elif axis == "v":
+        count = 0
+        for i in range(len(word)):
+            move(col, row+count)
+            count += 1
+            write_letter(word[i])
+
+def decode_command(string):
+    tokens = string.split(" ")
+    col = int(tokens[0])
+    row = int(tokens[1])
+    word = tokens[2]
+    axis = tokens[3]
+    write_word(col, row, word, axis)
+
 def move(col, row):
     turtle.goto(-300 + col * PIXEL_SIZE, 300 - row * PIXEL_SIZE)
 
 def main():
     initialize()
-    move(4,5)
-    write_letter("B")
+    #move(4,5)
+    #write_letter("B")
+    command = input("Enter a command: ")
+    decode_command(command)
     input()
 
 main()
