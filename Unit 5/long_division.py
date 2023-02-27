9b64d0e6ba4fcd88e281a6299639459732470685
Sam Sit <ss4521@rit.edu>
2023-02-27 13:09:21 -0500

    5.1.4
    doesn't match the answer, not sure what's wrong but me and another coworker deduced there might be a typo in the answer

--- Unit 5/long_division.py
+++ Unit 5/long_division.py
@@ -14,13 +14,13 @@ def precise_divison(n, d, p):
 def main():
     #print(precise_divison(25, 425, 25))
     print(precise_divison(2, 7, 25))
-    '''
+    
     para = input("Enter division parameters: ")
     tokens = para.split(" ")
     num = tokens[0]
     den = tokens[1]
     pre = tokens[2]
     print(precise_divison(num, den, pre))
-    '''
+    
 
 main()
