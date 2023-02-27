d57e96e40b156afb09aca0ecef1152469f9f35f8
Sam Sit <ss4521@rit.edu>
2023-02-27 13:45:50 -0500

    5.3.2 WIP HW

--- Unit 5/plots.py
+++ Unit 5/plots.py
@@ -4,6 +4,10 @@
     Author: Sam Sit
 '''
 
+import plotter
+import re
+import csv
+
 def quit():
     answer = input("Are you sure? (Y/N): ")
     if answer.lower() == "y":
@@ -11,8 +15,44 @@ def quit():
     else:
         return False
 
+def plot_grades(filename, firstname, lastname):
+    with open(filename) as file:
+        firstLine = ""
+        for line in file:
+            firstLine = line
+            flSplit = firstLine.split(",")
+            break
+    
+    #plotter.init("My Graph", "X-Axis", "Y-Axis")
+    plotter.init(flSplit[0], "Students", "Grades")
+
+    with open(filename) as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        regex = "[" + lastname + "]{" + str(len(lastname)) + "}"
+        studentFound = False
+        for line in csv_reader:
+            print(line[0])
+            if re.findall(regex, line[0]):
+                print("I AM HERE")
+                for number in line:
+                    try:
+                        plotter.add_data_point(float(number))
+                    except:
+                        continue
+                studentFound = True
+            else:
+                continue
+
+        if studentFound != True:
+            return False
+    
+    plotter.plot()
+    input("press enter to continue...")
+    return True
 
 def main():
+    '''
     while True:
         command = input(">> ")
         tokens = command.split()
@@ -22,9 +62,12 @@ def main():
                     break
         except IndexError:
             print("Enter a command or 'quit' to quit.")
-            
+
     print("Goodbye!")
+    '''
 
     #quit()
+
+    print(plot_grades("data/full_grades_010.csv", "Sion", "Lobasso"))
     
 main()
