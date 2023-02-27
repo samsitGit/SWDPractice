78e9fde6775bf0dc6bcda0543f627d09cde55444
Sam Sit <ss4521@rit.edu>
2023-02-27 13:19:47 -0500

    5.2.1 hw

---
+++ Unit 5/plots.py
@@ -0,0 +1,18 @@
+'''
+    5.2 Assignment
+
+    Author: Sam Sit
+'''
+
+def quit():
+    answer = input("Are you sure? (Y/N): ")
+    if answer.lower() == "y":
+        return True
+    else:
+        return False
+
+
+def main():
+    quit()
+    
+main()
