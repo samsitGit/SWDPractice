dcde70c6d36d57c06151e1e6b63cc93ac255a5e0
Sam Sit <ss4521@rit.edu>
2023-02-27 12:03:34 -0500

    5.22

--- Unit 5/csv_fun.py
+++ Unit 5/csv_fun.py
@@ -1,8 +1,10 @@
 '''
-    5.21 lecture
+    5.21-22 lecture
 
     Author: Sam Sit
 '''
+
+import csv
 
 def opener(filename):
     try:
@@ -12,9 +14,21 @@ def opener(filename):
     except:
         print("File cannot be opened:", filename)
         return False
-    
+
+def name_and_addresses(filename):
+    with open(filename) as file:
+        csv_reader = csv.reader(file)
+        #next(file)
+        next(csv_reader)
+        #for line in file:
+        for line in csv_reader:
+            #tokens = line.split(",")
+            #print("name:", tokens[0], "address:", tokens[1])
+            print("name:", line[0], "address:", line[1])
+
 def main():
-    filename = input("Enter a file name: ")
-    print(opener(filename))
+    #filename = input("Enter a file name: ")
+    #print(opener(filename))
+    name_and_addresses("data/full_grades_010.csv")
 
 main()
