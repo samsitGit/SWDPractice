47bd455ac84ac8d18aaec208f1c04e437b4d99f8
Sam Sit <ss4521@rit.edu>
2023-02-28 14:10:36 -0500

    5.2.4 WIP
    ignore previous commit, it was supposed to be 5.2.4 not 5.3.2

--- Unit 5/plots.py
+++ Unit 5/plots.py
@@ -16,30 +16,35 @@ def quit():
         return False
 
 def plot_grades(filename, firstname, lastname):
+    
+    ''' # to get first line
     with open(filename) as file:
         firstLine = ""
         for line in file:
             firstLine = line
             flSplit = firstLine.split(",")
             break
-    
+    '''
+
     #plotter.init("My Graph", "X-Axis", "Y-Axis")
-    plotter.init(flSplit[0], "Students", "Grades")
+    #plotter.init(flSplit[0], "Scores", "Grades")
+    plotter.init((firstname + " " + lastname), "Scores", "Grades")
 
     with open(filename) as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
-        regex = "[" + lastname + "]{" + str(len(lastname)) + "}"
+        regex = "[" + lastname + "]{" + str(len(lastname)) + "}\w[" + firstname + "]{" + str(len(firstname)) + "}"
         studentFound = False
         for line in csv_reader:
-            print(line[0])
             if re.findall(regex, line[0]):
-                print("I AM HERE")
+                count = 0
                 for number in line:
-                    try:
-                        plotter.add_data_point(float(number))
-                    except:
-                        continue
+                    count += 1
+                    if count > 3: #skip first 3 columns
+                        try:
+                            plotter.add_data_point(float(number))
+                        except:
+                            continue
                 studentFound = True
             else:
                 continue
@@ -68,6 +73,7 @@ def main():
 
     #quit()
 
-    print(plot_grades("data/full_grades_010.csv", "Sion", "Lobasso"))
+    #print(plot_grades("data/full_grades_010.csv", "Sion", "Lobasso"))
+    print(plot_grades("data/full_grades_010.csv", "Carlyne", "Myrman"))
     
 main()
