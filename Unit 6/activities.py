2870c7ba746734a2fbe6f01f3fc96ff0c50a888f
Sam Sit <ss4521@rit.edu>
2023-03-01 12:58:23 -0500

    6.1-2 lecture

---
+++ Unit 6/activities.py
@@ -0,0 +1,18 @@
+'''
+    6.1-2 lecture
+
+    Author: Sam Sit
+'''
+import arrays
+
+def making_arrays():
+    print(arrays.Array(5))
+    print(arrays.Array(1,0))
+    print(arrays.Array(10,""))
+    print(arrays.Array(20, False))
+
+def main():
+
+    making_arrays()
+
+main()
