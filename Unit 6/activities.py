dfbbfa1617064d941eb3fa6a1cd1a7ef7f581482
Sam Sit <ss4521@rit.edu>
2023-03-01 13:01:04 -0500

    6.3

--- Unit 6/activities.py
+++ Unit 6/activities.py
@@ -11,8 +11,20 @@ def making_arrays():
     print(arrays.Array(10,""))
     print(arrays.Array(20, False))
 
+def while_fill(an_array):
+    i = 0
+    while i < len(an_array):
+        an_array[i] = i
+        i += 1
+    print(an_array)
+
 def main():
 
-    making_arrays()
+    #making_arrays()
+
+    an_array = arrays.Array(10)
+    print(an_array)
+    while_fill(an_array)
+
 
 main()
