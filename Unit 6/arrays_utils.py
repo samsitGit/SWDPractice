31d5eaf81f92d517bf5029273326e8485655a180
Sam Sit <ss4521@rit.edu>
2023-03-02 14:30:06 -0500

    6.8

--- Unit 6/arrays_utils.py
+++ Unit 6/arrays_utils.py
@@ -16,11 +16,22 @@ def random_array(size, min_value=0, max_value=None):
         count += 1
     return array
 
+def range_array(start, stop, step=1):
+    a_range = range(start, stop, step)
+    a_array = arrays.Array(stop-start)
+    count = 0
+    for i in a_range:
+        a_array[count] = i
+        count += 1
+    return a_array
+
 def main():
     
     random.seed(1)
 
     print(random_array(10))
+
+    print(range_array(1, 10))
 
 
 main()
