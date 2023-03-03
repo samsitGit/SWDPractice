837153d5840eb8a9f1b616fc2ae1f12b9b9c87f6
Sam Sit <ss4521@rit.edu>
2023-03-03 12:17:02 -0500

    6.22 completed

--- Unit 6/searches.py
+++ Unit 6/searches.py
@@ -1,5 +1,5 @@
 '''
-    6.9 lecture
+    6.9, 6.21-22 lecture
 
     Author: Sam Sit
 '''
@@ -17,17 +17,30 @@ def linear_search(an_array, target):
     return None
 
 def binary_search(an_array, target, start=None, end=None):
+
     if start==None:
         start = 0
         end = len(an_array)
 
-    if start > end:
-        return None
+    elif start > end:
+        return "test"
+    
+    midpoint = (start+end)//2
+    mid = an_array[midpoint]
+    
+    if mid == target:    
+        return midpoint
+    elif mid < target:
+        return binary_search(an_array, target, midpoint+1, end)
+    elif mid > target:
+        return binary_search(an_array, target, start, midpoint-1)
 
 def main():
-    #array = arrays_utils.range_array(1,100)
-    #target = 50
+    array = arrays_utils.range_array(1,100)
+    target = 50
     #print(linear_search(array, target))
+    
+    print(binary_search(array, target))
     print()
 
 main()
