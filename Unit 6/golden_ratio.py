fb40f401e69d160363259bf081a1f9a9427f1ec8
Sam Sit <ss4521@rit.edu>
2023-03-03 12:50:48 -0500

    6.1.2 hw

--- Unit 6/golden_ratio.py
+++ Unit 6/golden_ratio.py
@@ -14,6 +14,11 @@ def fibonacci(n):
     if n > 2:
         return fibonacci(n-1) + fibonacci(n-2)
 
+def fill_fibonacci_array(array, index = 0):
+    if index < len(array):
+        array[index] = fibonacci(index)
+        fill_fibonacci_array(array, index+1)
+
 
 def main():
     n = 9
