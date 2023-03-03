bd5f3f813082380f36655618c555381aecfcb319
Sam Sit <ss4521@rit.edu>
2023-03-03 12:54:35 -0500

    6.1.3 hw

--- Unit 6/golden_ratio.py
+++ Unit 6/golden_ratio.py
@@ -3,6 +3,10 @@
 
     Author: Sam Sit
 '''
+
+import arrays
+
+#6.1
 def fibonacci(n):
     n = int(n)
     if n <= 0:
@@ -14,14 +18,25 @@ def fibonacci(n):
     if n > 2:
         return fibonacci(n-1) + fibonacci(n-2)
 
+#6.2
 def fill_fibonacci_array(array, index = 0):
     if index < len(array):
-        array[index] = fibonacci(index)
+        array[index] = fibonacci(index+1)
         fill_fibonacci_array(array, index+1)
-
+    return array
 
 def main():
+
+    #6.1
     n = 9
     print(fibonacci(n))
+
+    #6.3
+    size = 20
+    array = arrays.Array(size)
+    print(fill_fibonacci_array(array))
+    
+
+
 
 main()
