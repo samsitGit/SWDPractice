b4781deee22fbe807c6d341f46a117d8469906c9
Sam Sit <ss4521@rit.edu>
2023-03-10 17:26:39 -0500

    7.6 lecture
    fixed bug and completed 7.6

--- Unit 7/sorts.py
+++ Unit 7/sorts.py
@@ -27,12 +27,19 @@ def shift_wo_swap(an_array, index):
     target = an_array[index]
     target_index = index
 
-    for i in range(1, index):
+    for i in range(1, index+1):
         if target < an_array[index-i]:
+            swap(an_array, index-i, target_index)
             target_index -= 1
+
         else:
             break
-    swap(an_array, index, target_index)
+    
+    return an_array
+
+def insertion_sort_wo_swap(an_array):
+    for i in range(len(an_array)):
+        shift_wo_swap(an_array, i)
     return an_array
 
 
@@ -48,9 +55,12 @@ def main():
     #print(random)
     #print(insertion_sort(random))
 
-    new = [65, 79, 84, 86, 70, 63]
-    print(new)
-    print(shift_wo_swap(new, 4))
-    
+    #new = [65, 79, 84, 86, 70, 63]
+    #print(new)
+    #print(shift_wo_swap(new, 4))
+
+    random = array_utils.random_array(10)
+    print(random)
+    print(insertion_sort_wo_swap(random))
 
 main()
