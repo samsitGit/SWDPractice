fa2c7c6d953e79923e727f2d1a526e6d8dd0697d
Sam Sit <ss4521@rit.edu>
2023-05-16 16:52:55 -0400

    7.12 wip

--- GCIS 123/Unit 7/searches.py
+++ GCIS 123/Unit 7/searches.py
@@ -11,6 +11,7 @@ def linear_search(an_array, target):
             return index
     return None
 
+'''
 def binary_search(an_array, target, start=None, end=None):
     if start is None:
         start = 0
@@ -31,6 +32,7 @@ def binary_search(an_array, target, start=None, end=None):
         else:
             end = mid - 1
             return binary_search(an_array, target, start, end)
+'''
 
 #7.11
 def increasing_comparator(a, b):
@@ -42,6 +44,27 @@ def decreasing_comparator(a, b):
     if a > b:
         return True
     return False
+
+def binary_search(an_array, target, increasing_comparator, start=None, end=None):
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(an_array) - 1
+
+    if start > end:
+        return None
+    else:
+        mid = (start + end) // 2
+        # print("target =", target, "start =", start, "end =", end,
+        #     "mid =", mid, "value =", an_array[mid])
+        if an_array[mid] == target:
+            return mid
+        elif an_array[mid] < target:
+            start = mid + 1
+            return binary_search(an_array, target, start, end)
+        else:
+            end = mid - 1
+            return binary_search(an_array, target, start, end)
 
 def main():
     '''
