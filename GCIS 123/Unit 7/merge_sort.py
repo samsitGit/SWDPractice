5e42e86be12b276f674caa06730d233d1bd5ede8
Sam Sit <ss4521@rit.edu>
2023-05-17 00:11:23 -0400

    7.17

--- GCIS 123/Unit 7/merge_sort.py
+++ GCIS 123/Unit 7/merge_sort.py
@@ -3,10 +3,34 @@
     Author: Sam Sit
 '''
 
+import arrays
+import array_utils
+
 def merge_sort(an_array):
     if len(an_array) < 2:
         return an_array
 
+def split(an_array):
+    total = len(an_array)
+    
+    if total % 2:
+        evens = arrays.Array(int(total/2+1))
+    else:
+        evens = arrays.Array(int(total/2))
+    odds = arrays.Array(int(total/2))
+
+    for i in range(total):
+        if i%2:
+            print(i)
+            odds[int(i/2)] = an_array[i]
+        else:
+            evens[int(i/2)] = an_array[i]
+
+    return evens, odds
+
 def main():
-    print()
+
+    an_array = array_utils.range_array(0, 10)
+    print(split(an_array))
+
 main()
