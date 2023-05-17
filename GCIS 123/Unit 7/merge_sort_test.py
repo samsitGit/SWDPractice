16229ab1e29e00f5cd4ec3777964fc38f04e8ddc
Sam Sit <ss4521@rit.edu>
2023-05-17 19:07:25 -0400

    7.20 testings done

--- GCIS 123/Unit 7/merge_sort_test.py
+++ GCIS 123/Unit 7/merge_sort_test.py
@@ -95,17 +95,19 @@ def test_merge2():
 def test_merge3():
     #set up
 
-    sorted1 = arrays.Array(2)
+    sorted1 = arrays.Array(3)
     sorted1[0] = 1
     sorted1[1] = 3
+    sorted1[2] = 6
     sorted2 = arrays.Array(2)
     sorted2[0] = 2
     sorted2[1] = 4
-    expected = arrays.Array(4)
+    expected = arrays.Array(5)
     expected[0] = 1
     expected[1] = 2
     expected[2] = 3
     expected[3] = 4
+    expected[4] = 6
 
     #invoke
     result = merge_sort.merge(sorted1, sorted2)
