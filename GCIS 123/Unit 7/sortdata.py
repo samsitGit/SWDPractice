5cb6d7004667d1e6c01018c1ac3a1aaaa286a093
Sam Sit <ss4521@rit.edu>
2023-05-18 23:01:41 -0400

    7.1.1 wip

---
+++ GCIS 123/Unit 7/sortdata.py
@@ -0,0 +1,15 @@
+'''
+    7.1 Assignment - Naive Sorts
+    Author: Sam Sit
+'''
+
+import arrays
+import array_utils
+import sorts
+
+def sort_col(filepath, i_col, size):
+    print()
+
+def main():
+    sort_col("/data/dataset.csv", 2, 189)
+    sort_col("/data/dataset.csv", 7, 189)
