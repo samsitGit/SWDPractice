bc0125e6095ddc6c389d4efa3756e3868694936f
Sam Sit <ss4521@rit.edu>
2023-05-21 11:16:52 -0400

    7.2 assignment start

--- GCIS 123/Unit 7/sort_times.py
+++ GCIS 123/Unit 7/sort_times.py
@@ -7,6 +7,8 @@ import sorts
 import time
 import array_utils
 
+SIZES = array_utils.range_array(200,2001,300)
+
 ''' #pre 7.9
 def insertion_sort_function_timer(an_array):
     begin = time.perf_counter()
@@ -40,13 +42,17 @@ def sort_function_timer(an_array, sort_function):
     return elapsed
 
 
+
 def main():
-    
+    '''
     print("now testing with size 3000 arrays")
     #random elements
     array1 = array_utils.random_array(3000)
     array2 = array1
     print("insertion sort (swaps):", sort_function_timer(array1, sorts.insertion_sort))
     print("insertion sort (no swaps):", sort_function_timer(array2, sorts.insertion_sort_wo_swap))
+    '''
+    #7.2 assignment
+    print(SIZES)
 
 main()
