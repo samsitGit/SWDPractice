b13a8a6edd467258650fb1a6237872c9227d4510
Sam Sit <ss4521@rit.edu>
2023-05-24 14:02:13 -0400

    add profile

--- React.js Practice/my-app/src/App.js
+++ React.js Practice/my-app/src/App.js
@@ -1,6 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+
 function MyButton() {
   return (
     <button>test button</button>
@@ -16,6 +40,7 @@ function App() {
           Front page edit
         </p>
         <MyButton />
+        <Profile />
         <a
           className="App-link"
           href="https://reactjs.org"
