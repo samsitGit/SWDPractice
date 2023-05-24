153932020231ccc247bbfe5ac2332dffa9cabb6d
Sam Sit <ss4521@rit.edu>
2023-05-24 13:53:41 -0400

    nest test button

--- React.js Practice/my-app/src/App.js
+++ React.js Practice/my-app/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>test button</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Front page edit
         </p>
+        <MyButton />
         <a
           className="App-link"
           href="https://reactjs.org"
