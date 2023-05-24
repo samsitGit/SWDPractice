cc9427b7f1c54e6fc5f1b21996e95260ca09b1ac
Sam Sit <ss4521@rit.edu>
2023-05-24 14:48:17 -0400

    move up state

--- React.js Practice/my-app/src/Board/Board.js
+++ React.js Practice/my-app/src/Board/Board.js
@@ -1,18 +1,11 @@
 import "./styles.css"
 import { useState } from "react";
 
-function Square() {
-
-    const[value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue('X');
-    }
+function Square({value}) {
 
     return (
         <
             button className="square"
-            onClick={handleClick}
         >
             {value}
         </button>
@@ -20,22 +13,23 @@ function Square() {
 }
 
 export default function Board() {
+    const [squares, setSquares] = useState(Array(0).fill(null));
     return (
       <>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+            <Square value={squares[0]} />
+            <Square value={squares[1]} />
+            <Square value={squares[2]} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+            <Square value={squares[3]} />
+            <Square value={squares[4]} />
+            <Square value={squares[5]} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+            <Square value={squares[6]} />
+            <Square value={squares[7]} />
+            <Square value={squares[8]} />
         </div>
       </>
     );
