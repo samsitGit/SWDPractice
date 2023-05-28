41ca4e6cfd0ba1179d368f5a90d643faf70258db
Sam Sit <ss4521@rit.edu>
2023-05-28 19:56:40 -0400

    8.2.3

--- GCIS 123/Unit 8/idiots_delight.py
+++ GCIS 123/Unit 8/idiots_delight.py
@@ -10,3 +10,13 @@ def deal_hand():
     hand = c.deal_one_hand(deck, 4)
     return deck, hand
 
+def discard(hand, num):
+    if num != 2 or num != 4 or len(hand) < 4:
+        return hand
+    elif num == 4:
+        hand = []
+        return hand
+    elif num == 2:
+        hand.pop(1)
+        hand.pop(1)
+        return hand
