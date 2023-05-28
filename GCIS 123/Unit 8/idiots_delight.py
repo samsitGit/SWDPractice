10aa21a0476af8139e964e5e5d3e76e1295a4ba0
Sam Sit <ss4521@rit.edu>
2023-05-28 19:58:53 -0400

    8.2.4

--- GCIS 123/Unit 8/idiots_delight.py
+++ GCIS 123/Unit 8/idiots_delight.py
@@ -14,9 +14,17 @@ def discard(hand, num):
     if num != 2 or num != 4 or len(hand) < 4:
         return hand
     elif num == 4:
-        hand = []
+        hand = ()
         return hand
     elif num == 2:
         hand.pop(1)
         hand.pop(1)
         return hand
+    
+def play_round(deck, hand):
+    while len(hand) < 4:
+        c.draw(deck, hand)
+        print(hand)
+    c.draw(deck, hand)
+    print(hand)
+    return deck, hand
