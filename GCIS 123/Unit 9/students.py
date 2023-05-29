5c05e112bb890e299e761fb943046dda28aee729
Sam Sit <ss4521@rit.edu>
2023-05-29 19:13:29 -0400

    9.18

--- GCIS 123/Unit 9/students.py
+++ GCIS 123/Unit 9/students.py
@@ -23,6 +23,18 @@ def get_student(population, id):
         else:
             return None
 
+def add_credits(population, id, credits):
+    s = get_student(population, id)
+    if s != None:
+        s[2] += credits
+
+def get_credits(population, id):
+    s = get_student(population, id)
+    if s != None:
+        return s[2]
+    else:
+        return None
+
 def main():
     '''
     s1 = make_student(1, "Sam Sit")
@@ -40,8 +52,12 @@ def main():
     add_student(population, 2, "Sun Sit")
     add_student(population, 3, "San Sit")
     add_student(population, 4, "Sin Sit")
-
+    '''
     print(get_student(population, 1))
     print(get_student(population, 6))
+    '''
+    add_credits(population, 1, 50)
+    print(get_credits(population, 1))
+
 
 main()
