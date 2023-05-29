12b8c9a30c3ad54ec4d70f09e2ba25d6aad9680e
Sam Sit <ss4521@rit.edu>
2023-05-28 22:49:34 -0400

    9.3

--- GCIS 123/Unit 9/activities.py
+++ GCIS 123/Unit 9/activities.py
@@ -3,13 +3,23 @@
     Author: Sam Sit
 '''
 
+import arrays
+import timing
+
 def unique_array(a, v):
-    noneIndex = None
-    noneIndexFound = False
     for i in range(len(a)):
-        if a[i] == None and not noneIndexFound:
-            noneIndex = i
-            noneIndexFound = True
+        if a[i] == None:
+            a[i] = v
+            break
         elif a[i] == v:
             return
-    a[noneIndex] = v
+
+def fill_array(l):
+    a = arrays.Array(l)
+    for i in range(l-1):
+        unique_array(a, i)
+
+def main():
+    timing.time_function(fill_array, 5000)
+
+main()
