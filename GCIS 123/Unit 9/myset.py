5ca8ef4cd949747b6293d34e9ea5b92e635a8a73
Sam Sit <81936311+samsitGit@users.noreply.github.com>
2023-08-17 21:02:51 -0400

    9.28

--- GCIS 123/Unit 9/myset.py
+++ GCIS 123/Unit 9/myset.py
@@ -2,7 +2,21 @@ def make_myset(length, hash_func=hash):
     table = []
     for i in range(length):
         table.append([])
-    print(table)
     return (hash_func, table)
 
-make_myset(5)
+def add(myset, element):
+    index = myset[0](element) % len(myset[1])
+    if myset[1][index] == element:
+        pass
+    else:
+        myset[1][index] = element
+
+def main():
+    a = make_myset(7)
+    add(a, 5)
+    add(a, 2)
+    add(a, 1)
+    add(a, 2)
+    print(a)
+
+main()
