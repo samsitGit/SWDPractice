ad6b9c66e8dd528b26572f088facaedb7dc26594
Sam Sit <81936311+samsitGit@users.noreply.github.com>
2023-08-22 17:09:58 -0400

    9.29

--- GCIS 123/Unit 9/myset.py
+++ GCIS 123/Unit 9/myset.py
@@ -11,6 +11,13 @@ def add(myset, element):
     else:
         myset[1][index] = element
 
+def contains(myset, element):
+    index = myset[0](element) % len(myset[1])
+    if myset[1][index] == element:
+        return True
+    else:
+        return False
+
 def main():
     a = make_myset(7)
     add(a, 5)
@@ -18,5 +25,10 @@ def main():
     add(a, 1)
     add(a, 2)
     print(a)
+
+    print(contains(a, 4))
+    add(a,4)
+    print(contains(a, 4))
+
 
 main()
